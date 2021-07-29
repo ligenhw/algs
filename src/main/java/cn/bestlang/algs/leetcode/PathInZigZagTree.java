083f3f18a85b41fc6f95074992cc80b9cd46f0e7
gen <ligenhw@outlook.com>
2021-07-29 08:55:09 +0800

    add PathInZigZagTree

---
+++ src/main/java/cn/bestlang/algs/leetcode/PathInZigZagTree.java
@@ -0,0 +1,14 @@
+package cn.bestlang.algs.leetcode;
+
+import java.util.List;
+
+public class PathInZigZagTree {
+
+    public List<Integer> pathInZigZagTree(int label) {
+        return null;
+    }
+
+    private int findNum(int start, int num) {
+        return 3 * start - num - 1;
+    }
+}
