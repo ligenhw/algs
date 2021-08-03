eeee1ac3d78f13e4f13fc7056d30dccd243006b5
ligen <ligenhw@outlook.com>
2021-08-03 18:23:46 +0800

    schedule

---
+++ src/test/java/cn/bestlang/algs/interview/CounterLimiterTest.java
@@ -0,0 +1,28 @@
+package cn.bestlang.algs.interview;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CounterLimiterTest {
+
+    @Test
+    void acquire() throws InterruptedException {
+        CounterLimiter counterLimiter = new CounterLimiter(1000, 10);
+        while (true) {
+            boolean acquire = counterLimiter.acquire();
+            System.out.println("CounterLimiter : " + new Date() + ", " + acquire);
+            TimeUnit.MICROSECONDS.sleep(10);
+        }
+    }
+
+    @Test
+    void acquire2() throws InterruptedException {
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> System.out.println("test"), 0 , 1000, TimeUnit.MILLISECONDS);
+        TimeUnit.SECONDS.sleep(60);
+    }
+}
