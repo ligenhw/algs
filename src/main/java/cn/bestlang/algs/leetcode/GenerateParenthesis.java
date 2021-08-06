1edecb3f870398501953eedcc6acf2edce511c7a
gen <ligenhw@outlook.com>
2021-08-06 08:58:42 +0800

    NextPermutation

--- src/main/java/cn/bestlang/algs/leetcode/GenerateParenthesis.java
+++ src/main/java/cn/bestlang/algs/leetcode/GenerateParenthesis.java
@@ -1,10 +1,31 @@
 package cn.bestlang.algs.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParenthesis {
     public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, 0, 0, new StringBuilder(), n);
+        return result;
+    }
 
-        return null;
+    private void backtrack(List<String> result, int open, int close, StringBuilder current, int max) {
+        if (current.length() == max * 2) {
+            result.add(current.toString());
+            return;
+        }
+
+        if (open < max) {
+            current.append("(");
+            backtrack(result, open + 1, close, current, max);
+            current.deleteCharAt(current.length() - 1);
+        }
+
+        if (close < open) {
+            current.append(")");
+            backtrack(result, open, close + 1, current, max);
+            current.deleteCharAt(current.length() - 1);
+        }
     }
 }
---
+++ src/main/java/cn/bestlang/algs/leetcode/NextPermutation.java
@@ -0,0 +1,9 @@
+package cn.bestlang.algs.leetcode;
+
+public class NextPermutation {
+    public void nextPermutation(int[] nums) {
+        for (int i = nums.length - 1; i >= 0; i++) {
+
+        }
+    }
+}
